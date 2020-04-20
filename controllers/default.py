8cbd0930d031f16a2ad042383dfd172febe6c557
kriko dev <stephane.krikorian@gmail.com>
2020-04-20 18:55:25 +0000

    voici les changements
    04-20-2020 18-55

--- controllers/default.py
+++ controllers/default.py
@@ -5,9 +5,18 @@
 # -------------------------------------------------------------------------
 
 # ---- example index page ----
-def index():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+import git
+import csv
+import sqlite3
+import datetime
+
+DEFAULT_COUNTRY='France'
+#column_list_str=cache.ram('column_list_str', lambda: dict(content=str(columns[4:]).replace('[','').replace(']','')) , time_expire=60*60)
+column_list_str={}
+countries=[]
+kriko=1
+country_values={}
+
 
 def _init_log():
     import os,logging,logging.handlers,time
@@ -24,6 +33,219 @@ def _init_log():
     return logger
 
 app_logging = cache.ram('app_wide_log',lambda:_init_log(),time_expire=None)
+
+
+def index():
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+
+def compo_refresh_repo():
+    # refresh datas with git pull
+    # load database
+
+
+
+    application_path=request.env.app_folders
+    application=request.application
+    c='request.application}/static/data/'
+    #
+    path_list=[x for x in application_path if application in x]
+    path_final=f'{path_list[0]}/static/data/'
+    timestamp=datetime.datetime.now().strftime("%A, %d. %B %Y @ %H:%M:%S")
+    pull_status=f'refreshed on {timestamp}: {git.Git(path_final).pull()}'
+
+    # reload database
+
+    input_file=f'{path_final}/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+
+    # get columns names from input file
+    with open(input_file, newline='') as csvfile:
+         reader = csv.DictReader(csvfile)
+         colu=reader.fieldnames
+
+    # create database in memory with shared cache to reuse connection
+    con = sqlite3.connect("bimbamboum")
+    cur = con.cursor()
+
+    cur.execute(''' DROP TABLE IF EXISTS t ''')
+
+    # define columns of the table
+    columns=['Province', 'Country']
+    columns.extend(colu[2:])
+    
+
+    # construit la requete : chaine avec autant de colonnes que ds le csv  : (?,?,?,?,?)
+    ncol=len(columns)
+    vals=f'({("?,"*ncol)[:-1]})'
+
+
+    #global column_list_str
+    column_list_str=str(columns).replace('[','').replace(']','')
+    session.column_list_str=str(columns[4:]).replace('[','').replace(']','').replace(' ','') # on memorise une verson sans les 4 premiers champs    
+    #column_list_str=str(columns).replace('[','').replace(']','') 
+    #app_logging.info("global : %s" % column_list_str['content'])
+ 
+
+    # pas besoin des noms de colonnes
+    cur.execute("CREATE TABLE t (%s);" % column_list_str)
+        
+    # app_logging.info('Table does not exists.')
+    
+    #cur.execute("CREATE TABLE t ('Province', 'Country');") # use your column names here
+    # parse le fichier dans une liste de dictionnaires. 1 distionnaire = 1 ligne
+    with open(input_file,'r') as fin: # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.DictReader(fin) # comma is default delimiter
+        to_db = [ list(i.values()) for i in dr] # 
+        #to_db = [(i['Province/State'], i['Country/Region']) for i in dr]
+    # ce qui est important : avouir le bon nombre de colonnes 
+    cur.executemany("INSERT INTO t VALUES %s;" % vals, to_db)
+    #cur.executemany("INSERT INTO t ('Province', 'Country') VALUES (?, ?);", to_db)
+    con.commit()
+    #country_values=cache.ram('country_values',lambda: dict([(x[1],x[4:]) for x in cur.execute('select * from t ' )]))
+    #app_logging.info("creation for france: %s" % str(country_values['France']))
+
+    #countriesl=cache.ram('countriesl',lambda: countries)
+    #competitors=cache.ram('competitors', lambda: dict([(x.id,x.first_name.title()) for x in db(db.auth_user).select(db.auth_user.id, db.auth_user.first_name)]), time_expire=60*60)
+    #competitors=cache.ram('country_values', lambda: dict([(x[0],x[1] for x in db(db.auth_user).select(db.auth_user.id, db.auth_user.first_name)]), time_expire=60*60)
+    # database is loaded
+    cur=con.execute("SELECT Country FROM t WHERE Province='' ")
+    x=cur.fetchall()
+    countries=[c[0] for c in x]
+    session.countries=countries
+    #app_logging.info("creation : %s" % countries)
+    # creer la combo de selection du pays
+    #countries=db_select_countries(cur)
+
+    # countries : donner un nom du combo pour recuperer sa valeur dans le DOM!
+    flux_retour="""<select id='countries' onchange='var titi=document.getElementById("countries"); web2py_component("%s/"+titi.value,target="draw_ph");' > """ % URL("compo_get_array_dataset.load")
+    ic=0
+    for c in countries:
+        # selected = valeur de theme préselectionnée dans la combo. option du tag select
+        selected="selected='selected'>" if c == DEFAULT_COUNTRY else '>'
+        if selected != '>': pass # selectionné par défaut
+        #app_logging.info("str(c.id),selected,t.theme) = %s, %s, %s" % (str(t.id),selected,t.theme))
+        flux_retour+="""<option value='%s' %s %s</option>""" % (ic,selected,c)
+        ic+=1
+    flux_retour+="</select>"
+    XMLcon=XML(flux_retour)
+    # met à dispo de la communauté la liste des pays
+
+    return dict(pull_status=pull_status,country_count=len(countries),XMLcon=XMLcon)
+
+def db_select_countries():
+    # recupere de la base la liste des pays
+    con = sqlite3.connect("bimbamboum")
+    cur = con.cursor()
+    cur=con.execute("SELECT Country FROM t WHERE Province='' ")
+    x=cur.fetchall()
+    # app_logging.info(str(x))
+    return [c[0] for c in x]
+
+def db_select_values(location,lt='c'):
+    # recupere les valeurs d'un pays ou d'une region
+    con = sqlite3.connect("bimbamboum")
+    cur = con.cursor()
+    if lt=='c':
+        cur=con.execute(f"select * from t where Country ='{location}' and Province='' ")
+    else:
+        cur=con.execute(f"select * from t where Province='{location}' ")
+
+    # ignore les 4 premieres colonnes    
+    res=list(cur.fetchmany()[0])[4:]
+    app_logging.info("res=%s"%res)
+
+    return res
+
+    
+
+def compo_get_array_dataset():
+    # envoie les données pour le pays selectionné
+    global countries
+    
+    selected_country_id=int(request.args[0])
+
+    #countries=db_select_countries()
+    selected_country=session.countries[selected_country_id]
+    # graph datas
+    series=db_select_values(selected_country,'c')
+    #=["1,2,20,30,10,20","1,2,4,5,7,10"]
+    # passe de '2','3',.. a '2,3,4..'
+    
+    xdataset=session.column_list_str
+
+    app_logging.info("xda=%s"%type(xdataset))
+    xdattr=xdataset.replace(",",";").replace("'","").replace('/',',')
+    app_logging.info("xda TR=%s"% xdattr)
+    
+    ydataset=str([int(s) for s in series]).replace("]","").replace("[","").replace(' ','')
+    #xdataset="'1/2/20','1/3/20','1/4/20','1/5/20','1/6/20','1/7/20'"
+    #moment("12/25/1995", "MM-DD-YYYY");
+    #new Date(2017, 08, 16)
+    #xdataset="'1/22/20','1/23/20','1/24/20','1/25/20','1/26/20','1/27/20'"
+    xdataset="2017,08,16;2017,08,17;2017,08,18;2017,08,19;2017,08,20"
+    xdataset="1,22,20;1,23,20;1,24,20;1,25,20;1,26,20"
+    xdataset=xdattr
+    #ydataset="1,2,4,5,7,10"
+    #ydataset="0,1,3,5,7"
+    return dict(selected_country=selected_country,dataset=(xdataset,ydataset))
+
+def compo_refreshed_data():
+    # refresh datas with git pull
+    #w2p_path=request.env.applications_parent
+    app_logging.info("comminin from the cold")
+
+    x=db_select_countries()
+    return dict(refreshed_data=str(x))
+
+def dataim():
+
+    import csv
+
+    with open('files/names.csv', newline='') as csvfile:
+         reader = csv.DictReader(csvfile)
+         print(reader.fieldnames)
+         """
+         for row in reader:
+             p=row['Province/State']
+             c=row['Country/Region']
+             if (p and c):
+                print (p,c)
+         """
+
+@auth.requires_login()
+def ajax_cascade_province_only():
+    # NOT USED SO FAR
+    # specifique à la selection de la matiere et de la cascade sur le province
+    # appelé depuis la vue quand on change la valeur de la combo matiere
+    #
+    # on recupere la matiere
+    # on en déduit les provinces de la combo
+    #app_logging.info('ajax_cascade_province_collections matiere = %s' % request.vars.matiere_id)
+    #app_logging.info('ajax_cascade_province_collections province = %s' % request.vars.province_id)
+    provinces = db(db.province.matiere==request.vars.matiere_id).select(db.province.ALL,orderby=db.province.province)
+    #
+    # le province selectionné est soit le premier de la liste lorsque l'on a changé de matière [1]
+    # soit la valeur du province quand on a selectioné un nouveau province dans la combo [2}
+
+    sprovince_1= db.province[request.vars.province_id] if request.vars.province_id else provinces.first()
+    sprovince=sprovince_1.id
+
+    #app_logging.info('** ajax_cascade_province_collections province = %s' % sprovince)
+
+
+    # &&&& le specifique est la
+    result=tab_provinces(sprovince)
+    session.province_to_select=sprovince
+    session.matiere_to_select=db.province[sprovince].matiere
+
+    # le resultat est une combo avec 2 drop down matiere et theme
+    # reinjectée dans la vue
+
+    return XML(result)
+
+
 
 def grp():
     if request.env.request_uri[-1]=='/': redirect (URL())
@@ -50,11 +272,11 @@ def grp():
     pattern='web2py_component("%s"+"/"+$("#serie1").prop("checked")+"/"+$("#serie2").prop("checked"),target="%s");'
     # on appelle 3 composants tres semblables. ce qui les distingue : leur nom et la div dans laquelle ils injectent leur résultat
     # a chaque changement clic radio : load image, load titre du plantu , load code plantu
-    x=pattern % (URL("compo_change_serie.load"),"shadow_clone")
+    x=pattern % (URL("compo_change_serie.load"),"serie_ph")
     app_logging.info(x)
     form.element(_name='serie')['_onchange'] = x 
 
-    return dict(form=form,groupid=groupid)
+    return dict(form=form,groupid=groupid,countries=[],XMLcon="")
 
 def compo_change_serie():
     series=["1,2,20,30,10,20","1,2,4,5,7,10"]
